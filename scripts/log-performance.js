917e7be28bbcc607b318692c6a224f32d5f8795a
Peter Middis <middisp@gmail.com>
2015-06-22 15:31:01 +0100

    Tweaked log.perf

--- scripts/log-performance.js
+++ scripts/log-performance.js
@@ -13,12 +13,13 @@ var con = (function (module, opts) {
 		opts = opts || {};
 		var data = performance.getEntries(),
 			output = document.createElement('div'),
-			css = '#perf{background:' + (opts.background || '#FFF') + ';position:fixed;bottom:0;color:' + (opts.color || '#639') + ';left:0;padding:10px;overflow:auto;height:300px;z-index:1000;width:100%;line-height:20px;font-weight:bold}' +
-				'#perf .dr{width:100%; padding:5px;text-align:right}' +
+			elem, perfInit = 0,
+			css = '#perf{background:' + (opts.background || '#FFF') + ';position:fixed;bottom:0;color:' + (opts.color || '#639') + ';left:0;padding:10px;overflow:auto;height:200px;z-index:1000;width:100%;line-height:20px;font-weight:bold}' +
 				'#perf div{padding:5px;border-top:solid 1px}' +
 				'#perf div:nth-child(even){background-color:' + (opts.rowColor || '#eee') + '}' +
-				'#perf span{display:inline-block;width:24.9%}' +
-				'#perf .n {width:100%;overflow:hidden;text-overflow:ellipsis;white-space:nowrap;display:block;}',
+				'#perf span{width:100%;}' +
+				'#perf .n{overflow:hidden;text-overflow:ellipsis;white-space:nowrap;display:block;}' +
+				'#perf .dr{padding:5px;text-align:right}',
 			head = document.head || document.getElementsByTagName('head')[0],
 			style = document.createElement('style');
 
@@ -32,44 +33,76 @@ var con = (function (module, opts) {
 
 		head.appendChild(style);
 		output.setAttribute('id', 'perf');
+		output.setAttribute('data-action', 'micro');
 		document.body.appendChild(output);
+		elem = document.querySelector('#perf');
 	}
+
+	module.display = function (e) {
+		var target = e.target, id, action = this.getAttribute('data-action');
+		elem.innerHTML = '';
+		while (target.nodeName !== 'DIV') {
+			target = target.parentNode;
+		}
+		id = parseInt(target.getAttribute('data-id'));
+		if (action === 'micro') {
+			module.detail(id);
+			elem.setAttribute('data-action', 'macro');
+		} else {
+			module.perf();
+			elem.setAttribute('data-action', 'micro');
+		}
+	};
 
 
 	module.perf = function () {
 		if (window.performance.getEntries) {
-				var elem = document.querySelector('#perf'),
-				output = '', perfData = performance.timing,
-				domReady = perfData.loadEventEnd - perfData.navigationStart,
+			var output = '', perfData = performance.timing,
+				domComplete = perfData.domComplete - perfData.navigationStart,
 				connectTime = perfData.responseEnd - perfData.requestStart;
 
-			// Sort the results on start time;
-			data = data.sort(function (a, b) {
-				return parseFloat(a.responseStart) - parseFloat(b.responseStart);
-			});
+			// Sort the results on requestStart;
+			if (!perfInit) {
+				data = data.sort(function (a, b) {
+					return parseFloat(a.startTime) - parseFloat(b.startTime);
+				});
+			}
 
 			console.info(connectTime + 'ms');
-			output += '<span class="dr">DOM Ready: ' + domReady + 'ms</span>';
+			output += '<span class="dr">Requests: ' + data.length + ', Redirects: ' + performance.navigation.redirectCount + ', DOM Complete: ' + domComplete + 'ms</span>';
 
 			for (var i = 0; i < data.length; i++) {
 				output += '<div data-id="' + i + '">';
-				output += '<span class="n">' + data[i].name + '</span>';
-				output += '<span>' + data[i].responseStart.toFixed(2) /* Download Starts */ + 'ms</span>';
-				output += '<span>' + data[i].responseEnd.toFixed(2) /* Download Ends */ + 'ms</span>';
-				output += '<span>' + (data[i].responseEnd - data[i].responseStart).toFixed(2) /* Total Download Time */ + 'ms</span>';
-				output += '<span>' + data[i].duration.toFixed(2) /* End to End */ + 'ms</span>';
+				output += '<span class="n">' + data[i].name.substr(data[i].name.lastIndexOf('/') + 1) + '</span>';
 				output += '</div>';
 			}
 
 			elem.innerHTML = output;
+			if (!perfInit) {
+				elem.addEventListener('click', module.display);
+				perfInit = 1;
+			}
 		};
-		
-		module.getDetail = function(idx){
-			// TODO - Add detail: DNS lookup, open the connection, SSL, etc
+
+		module.detail = function (idx) {
+			var item = data[idx],
+				elem = document.querySelector('#perf'),
+				output = '<div>';
+
+			output += '<span class="n">' + item.name + '</span>';
+			output += '<span>Type: ' + item.initiatorType + '</span>';
+			if (item.requestStart === 0) {
+				output += '<span>Network: ' + (item.fetchStart - item.startTime).toFixed(3) + 'ms</span>';
+			} else {
+				output += '<span>Network: ' + (item.requestStart - item.startTime).toFixed(3) + 'ms</span>';
+			}
+			output += '<span>Request: ' + (item.responseStart - item.startTime).toFixed(3) + 'ms</span>';
+			output += '<span>Response: ' + (item.responseEnd - item.responseStart).toFixed(3) + 'ms</span>';
+
+			output += '</div>';
+			elem.innerHTML = output;
 		};
 	};
 
 	return module;
 } (con || {}));
-
-con.perf();
