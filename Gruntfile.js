b99e97b2d8f257cd043c9f465b6e1f72b5057e20
Peter Middis <middisp@gmail.com>
2015-07-30 10:57:23 +0100

    Updated to use LESS
    Lots of stuff going on here, check commit history.

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+﻿module.exports = function (grunt) {
+  require('jit-grunt')(grunt);
+
+  grunt.initConfig({
+   less: {
+      development: {
+        options: {
+          compress: false,
+          yuicompress: false
+        },
+        files: {
+          "styles/app.css": "less/app.less" // destination file and source file
+        }
+      },
+      production: {
+        options: {
+          compress: true,
+          yuicompress: true,
+          optimization: 2
+        },
+        files: {
+          "styles/app.min.css": "less/app.less" // destination file and source file
+        }
+      }
+    },
+    watch: {
+      styles: {
+        files: ['less/**/*.less'], // which files to watch
+        tasks: ['less'],
+        options: {
+          nospawn: true
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['less', 'watch']);
+};
