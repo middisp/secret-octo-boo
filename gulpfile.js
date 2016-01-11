9f768edaaa2526a6abbe98865a6a2a79647103ab
Peter Middis <middisp@gmail.com>
2016-01-11 10:57:43 +0000

    Re-adding in scss

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp'),
+ sass = require('gulp-sass'),
+ minifyCSS = require('gulp-minify-css'),
+ rename = require('gulp-rename'),
+ uglify = require('gulp-uglify'),
+ concat = require('gulp-concat'),
+ cache = require('gulp-cache'),
+ imageMin = require('gulp-imagemin');
+
+gulp.task('styles', function() {
+	gulp.src('sass/_main.scss')
+		.pipe(sass({})).on('error', sass.logError)
+		.pipe(rename({suffix: '.min'}))
+		.pipe(minifyCSS())
+		.pipe(gulp.dest('styles'));
+});
+
+gulp.task('scripts', function() {
+	gulp.src('scripts/utilities.js')
+		.pipe(rename({suffix: '.min'}))
+		.pipe(uglify())
+		.pipe(gulp.dest('scripts/test'));
+});
+
+gulp.task('images', function() {
+	gulp.src('img/orig/**/*')
+		.pipe(cache(imageMin({ optimizationLevel: 5, progressive: true, interlaced: true })))
+		.pipe(gulp.dest('img'));
+});
+
+gulp.task('watch', function() {
+	gulp.watch('sass/**/*.scss', ['styles']);
+	gulp.watch('img/orig/**/*', ['images']);
+});
+
+gulp.task('default', ['styles', 'scripts', 'images', 'watch']);
